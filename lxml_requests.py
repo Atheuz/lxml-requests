6259abb5ceb4254d558f8751f4ac6fa3de5fd0d2
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2012-08-28 03:40:48 +0200

    Undid src.py trick I want a single file to be able to move around, not several

--- lxml_requests.py
+++ lxml_requests.py
@@ -1,11 +1,32 @@
 # -*- coding: utf-8 -*-
 
-# Filename      lxml-requests.py
+# Filename      lxml_requests.py
 # Author        Lasse Vang Gravesen <gravesenlasse@gmail.com>
 # First edited  28-08-2012 01:30
-# Last edited   28-08-2012 03:02
+# Last edited   28-08-2012 03:38
 
-from src import *
+import lxml.html, lxml.etree
+import requests
+
+# HTML
+def html(url):
+    return lxml.html.fromstring(requests.get(url).text)
+
+def html_tostring(html):
+    return lxml.html.tostring(html, pretty_print=True)
+
+def html_fromstring(s):
+    return lxml.html.fromstring(s)
+
+# XML
+def xml(url):
+    return lxml.etree.fromstring(requests.get(url).text)
+
+def xml_tostring(xml):
+    return lxml.etree.tostring(xml, pretty_print=True)
+
+def xml_fromstring(s):
+    return lxml.etree.fromstring(s)
 
 requests.html = html
 requests.html_tostring = html_tostring
