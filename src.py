f054efe8bfbd46edd9d5a31054ef63862d2f0738
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2012-08-28 03:03:18 +0200

    Moved functions to src.py, and imported that
    Seems better for some reason.

---
+++ src.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Filename      src.py
+# Author        Lasse Vang Gravesen <gravesenlasse@gmail.com>
+# First edited  28-08-2012 03:02
+# Last edited   28-08-2012 03:02
+
+import lxml.html, lxml.etree
+import requests
+
+# HTML
+def html(url):
+    return lxml.html.fromstring(requests.get(url).text)
+
+def html_tostring(html):
+    return lxml.html.tostring(html, pretty_print=True)
+
+def html_fromstring(s):
+    return lxml.html.fromstring(s)
+
+# XML
+def xml(url):
+    return lxml.etree.fromstring(requests.get(url).text)
+
+def xml_tostring(xml):
+    return lxml.etree.tostring(xml, pretty_print=True)
+
+def xml_fromstring(s):
+    return lxml.etree.fromstring(s)
+
